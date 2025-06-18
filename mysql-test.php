545496e488f598f652be42384d951e41dc77b118
Sainithinkadarla <sainithin2004@gmail.com>
2025-06-18 15:54:44 +0530

    added checker code

--- mysql-test.php
+++ mysql-test.php
@@ -1,2 +1,2 @@
-<?php $conn = new mysqli("localhost", "admin", "yourpass", "test_project"); if ($conn->connect_error) { die("ERROR: Unable to connect: " . $conn->connect_error); } echo 'Connected to the dfgdatabase.
+<?php $conn = new mysqli("localhost", "admin", "yourpass", "test_project"); if ($conn->connect_error) { die("ERROR: Unable to connect: " . $conn->connect_error); } echo 'Connected to the database.
 '; $conn->close(); ?>
