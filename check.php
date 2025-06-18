b59060daac67e789949daf32df6ac4f25d9c5ebc
Sainithinkadarla <sainithin2004@gmail.com>
2025-06-18 15:50:41 +0530

    added checker code

--- check.php
+++ check.php
@@ -1,35 +1,50 @@
 <?php 
-require_once("config.php");
-//code check email
-if(!empty($_POST["emailid"])) {
-$uemail=$_POST["emailid"];
-$sql ="SELECT email FROM email_availabilty WHERE email=:email";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':email', $uemail, PDO::PARAM_STR);
-$query-> execute();
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
 
-if($query -> rowCount() > 0)
-echo "<span style='color:red'> Email Already Exit .</span>";
-else
- echo "<span style='color:green'> Email Available.</span>";
+header('Content-Type: application/json');
 
+$server ="localhost";
+$username = "admin";
+$password = "yourpass";
+$dbanme = "test_project";
+
+$conn = new mysqli($server, $username, $password, $dbanme);
+
+if ($conn -> connect_error){
+    error_log("Connection failed: " .$conn->connect_error);
+    echo json_encode(["error" => "database connection failed"]);
+    exit();
 }
-// End code check email
 
-//Code check user name
-if(!empty($_POST["username"])) {
-$username=$_POST["username"];
-$sql ="SELECT username FROM email_availabilty WHERE username=:username";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':username', $username, PDO::PARAM_STR);
-$query-> execute();
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
+$username_input = $_POST['username'];
 
-if($query -> rowCount() > 0)
- echo "<span style='color:red'> Username already exit .</span>";
-	else 
-echo "<span style='color:green'> Username Available.</span>";
+if (empty($username_input)){
+    echo json_encode(['error' => "username cannot be empty", 'available' => false]);
+    $conn->close();
+    exit();
 }
-// End code check username
+
+$sql = "select count(*) as count from logins where username = ?";
+$stmt = $conn ->prepare($sql);
+
+$stmt-bind_param('s', $username_input);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+
+$count = $row['count'];
+
+$response = [];
+
+if ($count > 0){
+    $response["available"] = false;
+}
+else{
+    $response["available"] = true;
+}
+
+
+echo json_encode($response);
+
+$stmt->close();
+$conn->close();
 ?>
